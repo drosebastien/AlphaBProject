46fd02ee7ccd0459180fdd504893cf7b13c3b78e
danteneo <danteneo@c0f7d829-3a5c-4080-ad29-62e1c14e6a05>
2012-02-06 13:34:49 +0000

    ajout de class pour le jeu Puissance 4

--- connectFour/PieceCF.java
+++ connectFour/PieceCF.java
@@ -1,19 +1,19 @@
-package morpion;
+package connectFour;
 
 import framework.*;
 
-public class PieceMorpion extends Piece {
-    public PieceMorpion(String name, int id) {
+public class PieceCF extends Piece {
+    public PieceCF(String name, int id) {
         super(name, id);
     }
 
     public boolean equals(Object obj) {
-        PieceMorpion piece = (PieceMorpion) obj;
+        PieceCF piece = (PieceCF) obj;
         return piece.getId() == getId();
     }
 
     public Piece clone() {
-        return new PieceMorpion(getName(), getId());
+        return new PieceCF(getName(), getId());
     }
 
     public String toString() {
