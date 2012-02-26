915701ba3613d1c990718c2226a22da49ccd38f9
danteneo <danteneo@c0f7d829-3a5c-4080-ad29-62e1c14e6a05>
2012-02-26 21:22:25 +0000

    Test de représentation d'un arbre dans le panel TreePanel. Dans les versions futur, le code devra être retiré pour être mis dans une autre classe.

---
+++ tools/Line.java
@@ -0,0 +1,35 @@
+package tools;
+/**
+ * Permet de représenté une droite y = ax + b par ses deux points a et b.
+ */
+public class Line {
+    private double a;
+    private double b;
+
+    public Line(double a, double b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public Line() {
+        this.a = 0;
+        this.b = 0;
+    }
+
+    public void setLine(int x1, int y1, int x2, int y2) {
+        a = ((y1 - y2) * 1.) / (x1 - x2);
+        b = y1 - x1 * a;
+    }
+
+    public double getY(double x) {
+        return a * x + b;
+    }
+
+    public double getX(double y) {
+        return (y - b) / a;
+    }
+
+    public String toString() {
+        return "y = " + a + "x + " + b;
+    }
+}
