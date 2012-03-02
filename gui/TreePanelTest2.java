235d9aeec25578e0db5baa74ae03d240b7b587c5
danteneo <danteneo@c0f7d829-3a5c-4080-ad29-62e1c14e6a05>
2012-03-02 22:49:14 +0000

    Modification de JTree Pourqu'il puisse mettre en évidence les noeuds.

--- gui/TreePanelTest2.java
+++ gui/TreePanelTest2.java
@@ -28,9 +28,57 @@ public class TreePanelTest2 extends TreePanel {
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
 
-        TreeNode root = Tree.makeTree(8, 2);
-        int[] tab = JTree.drawTree(20, root, g);
-        setPreferredSize(new Dimension(tab[0], 800));
+        //**
+        TreeNode root = Tree.makeTree(7, 2);
+        //*/TreeNode root = getTreeTest();
+
+        int rightMargin = JTree.drawTree(20, root, 40, 8, g);
+        setPreferredSize(new Dimension(rightMargin, 800));
+    }
+
+    //arbre de tests
+    public TreeNode getTreeTest() {
+        TreeNode root = new TreeNode(null);
+        //ajout du niv1
+        TreeNode lvl1_1 = new TreeNode(root);
+        TreeNode lvl1_2 = new TreeNode(root);
+        TreeNode lvl1_3 = new TreeNode(root);
+        root.addChildNode(lvl1_1);
+        root.addChildNode(lvl1_2);
+        root.addChildNode(lvl1_3);
+        //ajout du niv2
+        TreeNode lvl2_1 = new LeafNode(lvl1_1, 1);
+        TreeNode lvl2_2 = new LeafNode(lvl1_1, 1);
+        TreeNode lvl2_3 = new LeafNode(lvl1_1, 1);
+        TreeNode lvl2_4 = new TreeNode(lvl1_2);
+        TreeNode lvl2_5 = new LeafNode(lvl1_3, 1);
+        TreeNode lvl2_6 = new LeafNode(lvl1_3, 1);
+        lvl1_1.addChildNode(lvl2_1);
+        lvl1_1.addChildNode(lvl2_2);
+        lvl1_1.addChildNode(lvl2_3);
+        lvl1_2.addChildNode(lvl2_4);
+        lvl1_3.addChildNode(lvl2_5);
+        lvl1_3.addChildNode(lvl2_6);
+        //ajout du niv3
+        TreeNode lvl3_1 = new LeafNode(lvl2_4, 3);
+        TreeNode lvl3_2 = new TreeNode(lvl2_4);
+        TreeNode lvl3_3 = new LeafNode(lvl2_4, 3);
+        lvl2_4.addChildNode(lvl3_1);
+        lvl2_4.addChildNode(lvl3_2);
+        lvl2_4.addChildNode(lvl3_3);
+        //ajout du niv4
+        TreeNode lvl4_1 = new LeafNode(lvl3_2, 4);
+        TreeNode lvl4_2 = new LeafNode(lvl3_2, 5);
+        TreeNode lvl4_3 = new LeafNode(lvl3_2, 6);
+        TreeNode lvl4_4 = new LeafNode(lvl3_2, 7);
+        TreeNode lvl4_5 = new LeafNode(lvl3_2, 8);
+        lvl3_2.addChildNode(lvl4_1);
+        lvl3_2.addChildNode(lvl4_2);
+        lvl3_2.addChildNode(lvl4_3);
+        lvl3_2.addChildNode(lvl4_4);
+        lvl3_2.addChildNode(lvl4_5);
+
+        return root;
     }
 
     public void mouseClickedEvent(MouseEvent e) {
