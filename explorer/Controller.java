06c459066e4a315421859c0d3dfc0be864e56b31
danteneo <danteneo@c0f7d829-3a5c-4080-ad29-62e1c14e6a05>
2012-04-27 12:14:04 +0000

    Connect Four- Best node selection * sélection du meilleur noeud débuggé

--- explorer/Controller.java
+++ explorer/Controller.java
@@ -48,6 +48,7 @@ public class Controller implements GamePanelListener, TreePanelListener,
     }
 
     public void bestNodeSelected(boolean inExplorerMode, int[] pathToNode) {
+        quitPreview();
         pause(inExplorerMode);
         if(!inExplorerMode) {
             explorer.selectFirstNode(pathToNode);
