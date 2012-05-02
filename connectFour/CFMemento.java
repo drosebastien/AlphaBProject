6067e3a618dbc0fb479f64df7e73216c0a842d93
danteneo <danteneo@c0f7d829-3a5c-4080-ad29-62e1c14e6a05>
2012-05-02 13:46:31 +0000

    ConnectFour : isVictory() Correction du test de victoire avec l'exploration

--- connectFour/CFMemento.java
+++ connectFour/CFMemento.java
@@ -4,9 +4,12 @@ import framework.GameMemento;
 import framework.Board;
 import framework.Player;
 
+import java.util.ArrayList;
+
 public class CFMemento extends GameMemento {
     private Board gameBoard;
     private int player;
+    private CFMove[] setMoves;
 
     public void setBoardState(Board gameBoard) {
         this.gameBoard = gameBoard;
@@ -16,11 +19,26 @@ public class CFMemento extends GameMemento {
         this.player = player;
     }
 
+    public void setLastMove(ArrayList<CFMove> lastMoves) {
+        setMoves = new CFMove[lastMoves.size()];
+        for(int i = 0; i < lastMoves.size(); i++) {
+            setMoves[i] = lastMoves.get(i);
+        }
+    }
+
     public Board getBoardSavedState() {
         return gameBoard;
     }
 
     public int getCurrentPlayerSavedState() {
         return player;
+    }
+
+    public ArrayList<CFMove> getLastMove() {
+        ArrayList<CFMove> array = new ArrayList<CFMove>();
+        for(int i = 0; i < setMoves.length; i++) {
+            array.add(setMoves[i]);
+        }
+        return array;
     }
 }
