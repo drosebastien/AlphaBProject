0c49b50b31918e3cdb5605a261aa0533338cd603
danteneo <danteneo@c0f7d829-3a5c-4080-ad29-62e1c14e6a05>
2012-05-02 13:50:55 +0000

    Rev69 Résolution d'un problème de commit survenu lors du commit 69

---
+++ connectFour/evaluationFunction/CFEvalFunction.java
@@ -0,0 +1,12 @@
+package connectFour.evaluationFunction;
+
+import framework.*;
+import explorer.*;
+import connectFour.*;
+
+public class CFEvalFunction extends RandomEvalFunction {
+
+    public static String getName() {
+        return "Random CF Fct";
+    }
+}
