96f20ffedff7425113168ea0ffd2ce3efdc8aa0b
danteneo <danteneo@c0f7d829-3a5c-4080-ad29-62e1c14e6a05>
2012-05-29 23:05:34 +0000

    JavaDoc Ajout de documentation

--- explorer/RandomEvalFunction.java
+++ explorer/RandomEvalFunction.java
@@ -4,14 +4,33 @@ import framework.*;
 
 import java.util.Random;
 
+/**
+ * Cette classe represente une fonction d'evaluation qui attribue des valeurs
+ * aleatoire si les etats sont differents d'une situation de perte ou de
+ * victoire. Sinon, une victoire retourne une valeur max tandis qu'une defaite
+ * retourne une valeur minimale.
+ * @author Sebastien Drobisz.
+ */
 public class RandomEvalFunction implements EvalFunction {
 
+    /**
+     * Valeur attribue pour une victoire. Elle represente la plus grande valeur
+     * que la fonction puisse retourner.
+     */
     public static final int MAXVALUE = 50;
+    /**
+     * Valeur attribue pour une defaite. Elle represente la plus grande petite
+     * que la fonction puisse retourner.
+     */
     public static final int MINVALUE = -50;
     private Random gen;
     private Game game;
     private Player player;
 
+    /**
+     * Ce constructeur permet d'initialiser la fonction d'evaluation en
+     * creant un generateur de nombre aleatoire.
+     */
     public RandomEvalFunction() {
         gen = new Random();
     }
@@ -24,14 +43,32 @@ public class RandomEvalFunction implements EvalFunction {
         this.player = player;
     }
 
+    /**
+     * Cette methode permet de retourner le game qui lui a ete passe
+     * precedemment.
+     * @return Le jeu.
+     * @see #setGame(Game game)
+     */
     protected Game getGame() {
         return game;
     }
 
+    /**
+     * Cette methode permet de retourner le joueur qui lui a aete passe
+     * precedemment. (Joueur Max).
+     * @return Le joueur.
+     * @see #setPlayer(Player player)
+     */
     protected Player getPlayer() {
         return player;
     }
 
+    /**
+     * Cette fonction attribue des valeurs
+     * aleatoire si les etats sont differents d'une situation de defaite ou de
+     * victoire. Sinon, une victoire retourne une valeur max tandis qu'une defaite
+     * retourne une valeur minimale.
+     */
     public int evalFunction() {
 
         if(game.isFinish() && game.isVictory() &&
@@ -45,16 +82,28 @@ public class RandomEvalFunction implements EvalFunction {
         return gen.nextInt(99) - 49;
     }
 
+    /**
+     * Cette methode permet de retourner une description de ce que fait
+     * la fonction d'evaluation.
+     * @return La descritpion de ce que fait la fonction d'evaluation.
+     */
     public static String getDescription() {
         return "* -50 for oppenent victory\n" +
                "* 50 for root player victory\n" +
                "* otherwise random value in ]-50, 50[";
     }
 
+    /**
+     * Cette methode permet de retourner un nom qui est attribue a la classe.
+     * @return Le nom attribue a la fonction d'evaluation.
+     */
     public static String getName() {
         return "Random";
     }
 
+    /*
+     * Methodes de l'interface.
+     */
     public void started() {}
 
     public void moved(Movement move, int indexInTreeGame, MinMaxEvent event) {}
