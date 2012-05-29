6ccd0f4bff51fb2fa41a355720a147b0caaec14a
danteneo <danteneo@c0f7d829-3a5c-4080-ad29-62e1c14e6a05>
2012-05-29 23:05:34 +0000

    JavaDoc Ajout de documentation

--- trunk/explorer/Movement.java
+++ trunk/explorer/Movement.java
@@ -1,7 +1,21 @@
 package explorer;
 
+/**
+ * Cette enumeration permet de decrire le sens du mouvemetn que prend
+ * un algorithme.
+ * @author Sebastien Drobisz.
+ */
 public enum Movement {
+    /**
+     * Reste a la meme position.
+     */
     NEUTRAL,
+    /**
+     * Descend dans l'arbre.
+     */
     FORWARD,
+    /**
+     * Remonte dans l'arbre.
+     */
     BACKWARD;
 }
